5487ca10009fba41fc9fdad4a4219346768aa8d3
s0ur-oranges <91944643+s0ur-oranges@users.noreply.github.com>
2022-07-09 01:56:49 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,149 @@
+
+import torch
+import torch.nn as nn
+import torchvision
+import torchvision.transforms as transforms
+from torchvision.utils import save_image
+import numpy as np
+import matplotlib.pyplot as plt
+from datetime import datetime
+import os
+import time
+import streamlit as st
+
+
+transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize(mean=(0.5,),
+                         std=(0.5,))])
+
+train_dataset = torchvision.datasets.MNIST(
+    root='.',
+    train=True,
+    transform=transform,
+    download=True)
+
+batch_size = 128
+data_loader = torch.utils.data.DataLoader(dataset=train_dataset,
+                                          batch_size=batch_size,
+                                          shuffle=True)
+
+# Discriminator
+D = nn.Sequential(
+    nn.Linear(784, 512),
+    nn.LeakyReLU(0.2),
+    nn.Linear(512, 256),
+    nn.LeakyReLU(0.2),
+    nn.Linear(256, 1),
+    # nn.Sigmoid()
+)
+
+# Generator
+latent_dim = 100
+G = nn.Sequential(
+    nn.Linear(latent_dim, 256),
+    nn.LeakyReLU(0.2),
+    nn.BatchNorm1d(256, momentum=0.7),
+    nn.Linear(256, 512),
+    nn.LeakyReLU(0.2),
+    nn.BatchNorm1d(512, momentum=0.7),
+    nn.Linear(512, 1024),
+    nn.BatchNorm1d(1024, momentum=0.7),
+    nn.Linear(1024, 784),
+    nn.Tanh()
+)
+# Set device
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+D = D.to(device)
+G = G.to(device)
+
+# Loss and optimizers
+criterion = nn.BCEWithLogitsLoss()
+d_optimizer = torch.optim.Adam(D.parameters(), lr=0.0002, betas=(0.5, 0.999))
+g_optimizer = torch.optim.Adam(G.parameters(), lr=0.0002, betas=(0.5, 0.999))
+def scale_image(img):
+  out = (img + 1) / 2
+  return out
+
+
+if not os.path.exists('gan_images'):
+  os.makedirs('gan_images')
+# Training loop
+
+# labels to use in the loop
+ones_ = torch.ones(batch_size, 1).to(device)
+zeros_ = torch.zeros(batch_size, 1).to(device)
+
+# save losses
+d_losses = []
+g_losses = []
+
+for epoch in range(100):
+  for inputs, _ in data_loader:
+    # don't need targets
+
+    # reshape and move to GPU
+    n = inputs.size(0)
+    inputs = inputs.reshape(n, 784).to(device)
+
+    # set ones and zeros to correct size
+
+    ones = ones_[:n]
+    zeros = zeros_[:n]
+
+    # real images
+    real_outputs = D(inputs)
+    d_loss_real = criterion(real_outputs, ones)
+
+    # fake images
+    noise = torch.randn(n, latent_dim).to(device)
+    fake_images = G(noise)
+    fake_outputs = D(fake_images)
+    d_loss_fake = criterion(fake_outputs, zeros)
+
+    # gradient descent step
+    d_loss = 0.5 * (d_loss_real + d_loss_fake)
+    d_optimizer.zero_grad()
+    g_optimizer.zero_grad()
+    d_loss.backward()
+    d_optimizer.step()
+
+    for _ in range(2):
+        # fake images
+        noise = torch.randn(n, latent_dim).to(device)
+        fake_images = G(noise)
+        fake_outputs = D(fake_images)
+
+        # reverse the labels!
+        g_loss = criterion(fake_outputs, ones)
+
+        # gradient descent step
+        d_optimizer.zero_grad()
+        g_optimizer.zero_grad()
+        g_loss.backward()
+        g_optimizer.step()
+
+        # save losses
+    d_losses.append(d_loss.item())
+    g_losses.append(g_loss.item())
+
+  fake_images = fake_images.reshape(-1, 1, 28, 28)
+  save_image(scale_image(fake_images), f"gan_images/{epoch + 1}.png")
+
+
+
+def progressbar():
+    my_bar = st.progress(0)
+    for percent_complete in range(100):
+        time.sleep(0.01)
+        my_bar.progress(percent_complete + 1)
+
+st.title('Sentiment Analysis App')
+st.write('Performing Sentiment Analysis')
+
+if (st.sidebar.button('Predict Sentiment')):
+    progressbar()
+    st.image('gan_images/1.png')
+
+
+
